9c4b9d19a672022a68cc50c1954b651bc01f8a60
Kislitsina-Maria <ma.kis@mail.ru>
2022-05-24 15:03:47 +0300

    Second Changes

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ a = "My name is"
 name = 'Sarra'
 surname = 'Dankle'
 sum = 6 + 5
+if sum < 10:
+    print('not, yet')
+else:
+    print("No, I'm sorry")
 print(a, name, surname, ', Am I happy?')
 print(sum)
